6491a1c4af16b70a023bbb0428d2d3e9adb6ac75
Alfin <alfinbaiju@proton.me>
2025-01-26 11:41:36 +0530

    added items in cell and function to move items within cells

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,40 @@
 use macroquad::prelude::*;
-
 pub struct Cell {
-    id: (u32, u32),
+    pub id: (u32, u32),
     center: (f32, f32),
     origin: (f32, f32),
     color: Color,
     size: f32,
+    pub item: Option<Piece>,
 }
 
 impl Cell {
     pub fn draw(&self) {
         let (x, y) = self.origin;
         draw_rectangle(x, y, self.size, self.size, self.color);
+        if let Some(piece) = &self.item {
+            piece.draw(self.center);
+        }
     }
     pub fn highlight(&self) {
         let (x, y) = self.origin;
         draw_rectangle(x, y, self.size, self.size, Color::new(0.2, 1.0, 1.0, 0.25));
+    }
+
+    pub fn add(&mut self, piece: Piece) {
+        self.item = Some(piece);
+    }
+
+    pub fn select(&self) {
+        println!("{}", self.item.is_some());
+        if self.item.is_some() {
+            self.highlight();
+        }
+    }
+
+    pub fn move_item_to(&mut self, dest: &mut Cell) {
+        let piece = self.item.take();
+        dest.add(piece.unwrap());
     }
 }
 
@@ -40,6 +59,7 @@ impl Grid {
                     } else {
                         BLACK
                     },
+                    item: None,
                 });
             }
         }
@@ -57,17 +77,42 @@ impl Grid {
 
             let origin = (*x as f32 * w, *y as f32 * w);
             let center = (origin.0 + (w / 2.0), origin.1 + (w / 2.0));
-            *cell = Cell {
-                origin,
-                size: w,
-                center,
-                ..*cell
-            }
+            cell.origin = origin;
+            cell.center = center;
+            cell.size = w;
         }
     }
 
     pub fn get_cell(&self, x: u32, y: u32) -> &Cell {
         self.cells.get((x * 8 + y) as usize).unwrap()
+    }
+    pub fn get_cell_mut(&mut self, x: u32, y: u32) -> &mut Cell {
+        self.cells.get_mut((x * 8 + y) as usize).unwrap()
+    }
+
+    pub fn get_cell_mut_pair(
+        &mut self,
+        cell1: (u32, u32),
+        cell2: (u32, u32),
+    ) -> (&mut Cell, &mut Cell) {
+        let cell1_idx: u32 = cell1.0 * 8 + cell1.1;
+        let cell2_idx: u32 = cell2.0 * 8 + cell2.1;
+        let mid: u32;
+        if cell1_idx < cell2_idx {
+            mid = cell1_idx + 1;
+            let (g1, g2) = self.cells.split_at_mut(mid as usize);
+            (
+                g1.get_mut((mid - 1) as usize).unwrap(),
+                g2.get_mut((cell2_idx - mid) as usize).unwrap(),
+            )
+        } else {
+            mid = cell2_idx + 1;
+            let (g1, g2) = self.cells.split_at_mut(mid as usize);
+            (
+                g2.get_mut((cell1_idx - mid) as usize).unwrap(),
+                g1.get_mut((mid - 1) as usize).unwrap(),
+            )
+        }
     }
 
     pub fn draw(&self) {
@@ -76,12 +121,12 @@ impl Grid {
         }
     }
     //
-    pub fn coord_to_cell(&self, mouse_coords: (f32, f32)) -> Option<&Cell> {
+    pub fn coord_to_cell_id(&self, mouse_coords: (f32, f32)) -> Option<(u32, u32)> {
         let w = self.cell_size;
         let (xm, ym) = mouse_coords;
         let (x, y): (u32, u32) = ((ym / w).floor() as u32, (xm / w).floor() as u32);
         if (x < 8) & (y < 8) {
-            Some(self.get_cell(x, y))
+            Some((x, y))
         } else {
             None
         }
@@ -95,5 +140,20 @@ pub fn get_cell_width() -> f32 {
         w / 8.0
     } else {
         h / 8.0
+    }
+}
+pub enum Side {
+    Black,
+    White,
+}
+pub struct Piece {
+    pub name: String,
+    pub side: Side,
+}
+
+impl Piece {
+    pub fn draw(&self, origin: (f32, f32)) {
+        let (x, y) = origin;
+        draw_circle(x, y, 4.0, GREEN);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,57 @@
-use chess::{get_cell_width, Cell, Grid};
+use chess::{get_cell_width, Grid, Piece, Side};
 use macroquad::prelude::*;
 
 #[macroquad::main("My Game")]
 async fn main() {
     let mut cell_width = get_cell_width();
     let mut grid = Grid::new64(cell_width);
-    let mut highlight_cell: Option<&Cell> = None;
+    let mut selected_cell: Option<(u32, u32)> = None;
+    grid.get_cell_mut(0, 0).add(Piece {
+        name: String::from("tesT"),
+        side: Side::White,
+    });
     loop {
         if cell_width != get_cell_width() {
-            highlight_cell = None;
+            selected_cell = None;
             cell_width = get_cell_width();
             grid.resize(cell_width);
         }
+        left_click_handler(&mut grid, &mut selected_cell);
         grid.draw();
-        if is_mouse_button_pressed(MouseButton::Left) {
-            highlight_cell = grid.coord_to_cell(mouse_position());
-        }
-
-        if let Some(cell) = highlight_cell {
-            cell.highlight();
+        if let Some(cell) = selected_cell {
+            grid.get_cell(cell.0, cell.1).select();
         }
 
         next_frame().await
+    }
+}
+
+fn left_click_handler(grid: &mut Grid, selected_cell: &mut Option<(u32, u32)>) {
+    match selected_cell {
+        Some(selected) => {
+            if is_mouse_button_pressed(MouseButton::Left) {
+                let Some(dest) = grid.coord_to_cell_id(mouse_position()) else {
+                    return;
+                };
+                if dest == *selected {
+                    *selected_cell = None;
+                    return;
+                }
+                let (selected_c, dest_c) = grid.get_cell_mut_pair(*selected, dest);
+                selected_c.move_item_to(dest_c);
+                *selected_cell = None;
+            }
+        }
+        None => {
+            if is_mouse_button_pressed(MouseButton::Left) {
+                let Some(cell) = grid.coord_to_cell_id(mouse_position()) else {
+                    return;
+                };
+                if grid.get_cell(cell.0, cell.1).item.is_none() {
+                    return;
+                };
+                *selected_cell = Some(cell);
+            }
+        }
     }
 }
